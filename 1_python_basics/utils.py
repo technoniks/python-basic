8073d07cba6de9343be65f3e6f9882e6e1333c2c
niks <technoniks007@gmail.com>
2026-09-01 06:50:58 +0530

    time_it func added

--- 1_python_basics/utils.py
+++ 1_python_basics/utils.py
@@ -1,7 +1,16 @@
 from dotenv import load_dotenv
-import os
+import os, time
 
 load_dotenv()
 
 def loadGoogleApiKey():
   return os.getenv("GOOGLE_API_KEY")
+
+def time_it(func):
+  def wrapper(*args, **kwargs):
+    start = time.time()
+    result = func(*args, **kwargs)
+    end = time.time()
+    print(f"{func.__name__} took {str((end-start)*1000)} milliseconds")
+  
+  return wrapper
